e2af28dabd9acece4934b4aa3ed613d460c412aa
Vo-A-Qui <vohqui51202@icloud.com>
2024-01-27 15:10:36 +0700

    bai1_lab5

---
+++ src/demo/Demo11_font.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Demo11_font = () => {
+  return (
+    <View>
+      <Text>
+      React Native là các doan code dà dugo viêt san (framework) do công ty công nghê
+Facebook phát trién. Cac läp trinh viên React Native là nguöi sü dung nhüng framework này de phát trién nên các hê thông, nên täng üng dung trên các hê diêu hänh nhu IOS và Android. Ngôn ngü läp trinh duo sü dung nhi@u nhät là Javascript.
+      </Text>
+    </View>
+  )
+}
+
+export default Demo11_font
+
+const styles = StyleSheet.create({}) 
