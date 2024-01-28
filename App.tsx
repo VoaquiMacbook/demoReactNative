a171c20db1633a35df67ecc71bd32c7c3832260f
Vo-A-Qui <vohqui51202@icloud.com>
2024-01-29 04:12:20 +0700

    ok

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ import Demo2 from './src/demo/Demo2';
 import Demo3 from './src/demo/Demo3';
 import Demo4 from './src/demo/Demo4';
 import Demo4_1 from './src/demo/Demo4_1';
-import Demo5 from './src/demo/Demo5';
+import Demo5 from './src/demo/Demo5_ImgURL';
 import Demo6 from './src/demo/Demo6';
 import Demo7 from './src/demo/Demo7';
 import Demo_secureTextEtry from './src/demo/Demo_secureTextEtry';
@@ -40,6 +40,7 @@ import Bai1_lab5 from './labs/lab5/Bai1_lab5';
 import Bai2_lab5 from './labs/lab5/Bai2_lab5';
 import Bai3_lab5 from './labs/lab5/Bai3_lab5';
 import Bai4_lab5 from './labs/lab5/Bai4_lab5';
+import Bai1_lab6 from './labs/lab6/Bai1_lab6';
 // import assignment
 import Asm1 from './assignment/Asm1';
 
@@ -51,17 +52,25 @@ function App(): React.JSX.Element {
   return (
     <SafeAreaView>
       <View >
-      <StatusBar translucent backgroundColor="rgba(0,0,0,0)" />
-
+        <StatusBar translucent backgroundColor="rgba(0,0,0,0)" />
         {/* <Text style={styles.welcome}>Võ A Qui</Text> */}
-        {/* <Asm1></Asm1> */}
-        {/* <Bai2_lab5 /> */}
-      {/* <Bai3_lab5/> */}
-      <Bai4_lab5/>
 
+        {/* <Asm1></Asm1> */}
+
+        {/* <Bai1_lab5/> */}
+        {/* <Bai2_lab5 /> */}
+        {/* <Bai4_lab5 /> */}
+
+        <Bai1_lab6/>
+        
       </View>
     </SafeAreaView>
 
+
+
+
+
+    // bai2_lab5
 
     // <ImageBackground
     //     style={styles.background2}
@@ -84,41 +93,7 @@ function App(): React.JSX.Element {
     //     </View>
     //   </ImageBackground>
 
-    // <View style={styles.container}>
-    //   {/* Phần Header */}
-      
-    //   <View style={styles.header}>
-    //     <Image
-    //       source={{ uri: 'https://jpboxinggym.com/wp-content/uploads/2023/06/solo-female-traveller-bridge-1366x2048.jpg' }}
-    //       style={styles.headerImage}/>
-    //     {/* <TouchableOpacity style={styles.likeIcon} >
-    //       <Image source={require('./assets/images/google.png')}/>
-    //     </TouchableOpacity> */}
-    //   </View>
 
-    //   {/* Phần Body */}
-      
-    //   <View style={styles.body}>
-    //     {/* Nội dung chi tiết chuyến đi */}
-    //     <TouchableOpacity style={styles.likeIcon} >
-    //       <Image source={require('./assets/images/google.png')}/>
-    //     </TouchableOpacity>
-    //     <Text>Quảng Nam</Text>
-    //     <Text style={{marginTop:12, fontWeight:'bold'}}>Thông tin chuyến đi</Text>
-    //     <Text numberOfLines={6} ellipsizeMode="tail" style={styles.tripInfo}>
-    //       Höi An là m§t thanh phö truc thude tinh Quang Nam, Viet Nam. Phô co Höi An tung là möt thurong cang quôc te säm uät, göm nhüng di san kien trúc dä co tur häng träm nam truóc, duroc UNESCO cong nhan là di sán vän hãa the giói tù näm 1999.Höi An là mot thanh phö truc thuôc tinh Qung Nam, Vi@t Nam. Phö co Höi An türng
-    //     </Text>
-    //   </View>
-
-    //   {/* Phần Footer */}
-    //   <View style={styles.footer}>
-    //     {/* Giá tiền và nút đặt */}
-    //     <Text style={styles.price}>$100/Ngày</Text>
-    //     <TouchableOpacity style={styles.bookButton}>
-    //       <Text style={styles.bookButtonText}>Đặt ngay</Text>
-    //     </TouchableOpacity>
-    //   </View>
-    // </View>
   );
 };
 
@@ -176,68 +151,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
   },
 
-  container: {
-    flex: 1,
-  },
-  header: {
-    flex: 7,
-    position: 'relative',
-  },
-  headerImage: {
-    flex: 1,
-    width: '100%',
-    resizeMode: 'cover',
-  },
-  likeIcon: {
-    position: 'absolute',
-    right: 20,
-    width:'10%'
-    // Icon styles
-  },
-  body: {
-    flex: 3,
-    padding: 20,
-    position: 'absolute',
-    marginVertical:'130%',
-    width:'100%',
-    height:'40%',
-    backgroundColor:'white',
-    borderRadius:40,
-    paddingTop:40,
-  },
-  tripInfo: {
-    // Styles for trip information
-    marginTop:20,
-    fontSize:16,
-    fontFamily:'Roboto-ThinItalic',
-  },
-  footer: {
-    height: 80,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#0833AB', // Màu nền của Footer
-    paddingHorizontal: 20,
-    borderRadius:15,
-  },
-  price: {
-    // Styles for price text
-   color:'white',
-   fontWeight:'bold'
-  },
-  bookButton: {
-    // Styles for book button
-    backgroundColor:'white',
-    padding:10,
-    width:110,
-    borderRadius:10,  
-   
-  },
-  bookButtonText: {
-    // Styles for book button text
-    marginHorizontal:'15%',
-    
-  },
+
 
 
 });
---
+++ labs/lab6/Bai1_2_lab6.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Bai1_2_lab6 = () => {
+  return (
+    <View>
+      <Text>Bai1_2_lab6</Text>
+    </View>
+  )
+}
+
+export default Bai1_2_lab6
+
+const styles = StyleSheet.create({})
