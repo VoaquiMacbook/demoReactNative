7c1b91c3e3d953641af0f59fa910829a0d2fe715
Vo-A-Qui <vohqui51202@icloud.com>
2024-01-29 03:45:29 +0700

    lad5_hoanThanh

--- labs/lab5/Bai4_lab5.js
+++ labs/lab5/Bai4_lab5.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ImageBackground, StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
 
 const Bai4_lab5 = () => {
+  const [securePassword, setSecurePassword] = useState(true)
   return (
     <View>
-
-
       <ImageBackground source={{ uri: 'https://scontent.fsgn2-5.fna.fbcdn.net/v/t39.30808-6/423672434_1623061655101384_6595356168272493585_n.jpg?stp=dst-jpg_s1080x2048&_nc_cat=104&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=mos980koSWoAX8Hnz8G&_nc_ht=scontent.fsgn2-5.fna&oh=00_AfC6jd_8JDIHXc9lhzt_zeGfNnEKjSJMdol_vGG7s1Z6_w&oe=65BB5E15' }}>
 
         <View
@@ -21,7 +20,6 @@ const Bai4_lab5 = () => {
             <View style={[styles.horizontal, { justifyContent: 'space-between' }]}>
               <Image style={[styles.img_icon]} source={require('../../assets/images/arrow_left.png')} />
               <Image style={styles.img_icon} source={require('../../assets/images/ic_bacham.png')} />
-
             </View>
           </View>
 
@@ -44,12 +42,30 @@ const Bai4_lab5 = () => {
               </Text>
             </View>
             <View style={[styles.footer]}>
-              <Text style={[styles.txt_color,]}>❤️</Text>
+              <View style={[styles.horizontal, { justifyContent: 'space-between' }]}>
+                <Text style={styles.txt_rice_ft}>$100/ngày</Text>
+                <TouchableOpacity style={styles.btn_ft}>
+                  <Text style={styles.btn_txt_ft}>Đặt ngay</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </View>
-        <TouchableOpacity style={styles.ic_tim}>
-          <Text style={styles.ic_txt_tim} >❤️</Text>
+        <View style={styles.ic_sao}>
+          <View style={styles.horizontal}>
+            <Text style={styles.ic_txt_sao}>⭐️</Text>
+            <Text style={styles.txt_sao}>5.0</Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          onPress={() => setSecurePassword(!securePassword)}
+          style={styles.ic_tim}>
+          {
+            securePassword ?
+              <Text style={styles.ic_txt_tim} >🤍</Text>
+              :
+              <Text style={styles.ic_txt_tim} >❤️</Text>
+          }
         </TouchableOpacity>
       </ImageBackground>
     </View>
@@ -84,10 +100,9 @@ const styles = StyleSheet.create({
   img_icon: {
     tintColor: 'white',
     margin: '5%',
-  },
-  img_icon: {
-    tintColor: 'white',
-    margin: '5%',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 20,
   },
   // custom quangNam
   img_color2: {
@@ -108,12 +123,13 @@ const styles = StyleSheet.create({
   txt_name: {
     color: 'white',
     margin: '4%',
-    fontSize: 30,
-    fontWeight: 'bold',
+    fontSize: 26,
+    fontWeight: '700',
   },
 
   header: {
     flex: 3,
+    // height: '47%'
   },
   // custom body
   body: {
@@ -130,6 +146,8 @@ const styles = StyleSheet.create({
     height: 80,
     width: '100%',
     bottom: 0,
+    position: 'absolute',
+    padding: '4%',
   },
   tripInfo: {
     // Styles for trip information
@@ -142,7 +160,7 @@ const styles = StyleSheet.create({
   ic_tim: {
     width: '18%',
     height: '9%',
-    borderRadius:50,
+    borderRadius: 50,
     position: 'absolute',
     marginHorizontal: 330,
     marginVertical: 410,
@@ -152,10 +170,54 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     shadowColor: '1px 2px 9px #F4AAB9',
   },
-  ic_txt_tim:{
-    alignSelf:'center',
-    marginVertical:'20%',
-    fontSize:40,
+  ic_sao: {
+    width: '10%',
+    height: '6%',
+    position: 'absolute',
+    marginHorizontal: 330,
+    marginVertical: 370,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+    shadowColor: '1px 2px 9px #F4AAB9',
+
+  },
+  ic_txt_sao: {
+    alignSelf: 'center',
+    marginVertical: '26%',
+    fontSize: 18,
+    shadowOpacity: 1,
+    shadowRadius: 20,
+  },
+  txt_sao:{
+    alignSelf: 'center',
+    marginVertical: '26%',
+    fontSize: 18,
+    shadowOpacity: 1,
+    shadowRadius: 20,
+    color:'white',
+    fontWeight:'700'
+  },
+  ic_txt_tim: {
+    alignSelf: 'center',
+    marginVertical: '20%',
+    fontSize: 40,
+  },
+  // custom footer
+  btn_ft: {
+    backgroundColor: 'white',
+    width: '35%',
+    height: '130%',
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  btn_txt_ft: {
+    alignSelf: 'center',
+    fontWeight: '600',
+  },
+  txt_rice_ft: {
+    color: 'white',
+    fontWeight: '900',
   }
 });
 
