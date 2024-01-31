b70424b22c6d4775361cb1d9b8d87199318773a7
Vo-A-Qui <vohqui51202@icloud.com>
2024-02-01 00:43:55 +0700

    demo_TopTap ok

--- src/demo/Demo_TopTap.js
+++ src/demo/Demo_TopTap.js
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const Demo_TopTap = () => {
+function HomeScreen() {
   return (
-    <View>
-      <Text>Demo_TopTap</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
-  )
+  );
 }
 
-export default Demo_TopTap
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
 
-const styles = StyleSheet.create({})
+const Tab = createMaterialTopTabNavigator();
+
+export default function Demo_TopTap() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
